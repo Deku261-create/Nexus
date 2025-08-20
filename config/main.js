754ca7820e28312070d64e10f99526767aa935b8
Deku261-create <aneeshmidoriya26@gmail.com>
2025-08-20 18:47:11 -0500

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,14 +1,12 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
+var sitename = "Nexus"; // Change this to change the name of your website.
 var subtext = "v1.2"; // set the subtext
 
 // more settings in main.css
 
-
-
 // END CONFIG
-// DO NOT MODIFY IF YOU DO NOT KNOW WHAT YOUR DOING!
+// DO NOT MODIFY IF YOU DO NOT KNOW WHAT YOU'RE DOING!
 
 import "/./config/custom.js";
 
@@ -41,7 +39,6 @@ function displayFilteredGames(filteredGames) {
   });
 }
 
-
 function handleSearchInput() {
   const searchInputValue = document
     .getElementById("searchInput")
@@ -52,7 +49,6 @@ function handleSearchInput() {
   displayFilteredGames(filteredGames);
 }
 
-
 fetch("./config/games.json") 
   .then((response) => response.json())
   .then((data) => {
@@ -61,12 +57,9 @@ fetch("./config/games.json")
   })
   .catch((error) => console.error("Error fetching games:", error));
 
-
 document
   .getElementById("searchInput")
   .addEventListener("input", handleSearchInput);
 
 document.getElementById("title").innerHTML = `${sitename}`;
-
-document.getElementById("subtitle").innerHTML = `${subtext}`
-
+document.getElementById("subtitle").innerHTML = `${subtext}`;
